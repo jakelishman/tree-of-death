4c99818bdfe515ee6b1fd9c006e8ec7ec2c2f479
Jake Lishman <j.lishman@warwick.ac.uk>
2015-08-22 14:30:44 +0100

    Make Random.fraction nicer

--- TreeOfDeath/Random.fs
+++ TreeOfDeath/Random.fs
@@ -7,9 +7,15 @@ module Random =
     /// Random number class for use throughout the module.
     let private randomGenerator = System.Random()
 
+    /// Get an integer between Int32.MinValue and Int32.MaxValue.
+    let integer () = randomGenerator.Next (Int32.MinValue, Int32.MaxValue)
+
+    /// Get an integer between the two specified values.
+    let integerBetween a b = randomGenerator.Next (a, b)
+
     /// Get a random number between -1 and 1.
     let fraction () =
-        (2.0 * float (randomGenerator.Next ()) / float Int32.MaxValue) - 1.0
+        (float <| integerBetween (Int32.MinValue + 1) Int32.MaxValue) / float Int32.MaxValue
 
     /// Get a random non-negative fraction between 0 and 1.
     let nonNegativeFraction () =
@@ -17,9 +23,3 @@ module Random =
 
     /// Get a non-negative integer between 0 and Int32.MaxValue.
     let nonNegativeInteger () = randomGenerator.Next ()
-
-    /// Get an integer between Int32.MinValue and Int32.MaxValue.
-    let integer () = randomGenerator.Next (Int32.MinValue, Int32.MaxValue)
-
-    /// Get an integer between the two specified values.
-    let integerBetween a b = randomGenerator.Next (a, b)
