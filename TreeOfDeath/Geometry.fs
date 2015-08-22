34f3f0f87167b574a609e7855de97d952caeb38c
Jake Lishman <j.lishman@warwick.ac.uk>
2015-08-22 14:23:16 +0100

    Use Vertex module accessors in Geoemetry

--- TreeOfDeath/Geometry.fs
+++ TreeOfDeath/Geometry.fs
@@ -3,9 +3,11 @@
 module Geometry =
     /// Calculate the absolute distance between two points.
     let distanceBetween a b =
-        sqrt <| (float b.X - float a.X) ** 2.0 + (float b.Y - float a.Y) ** 2.0
+        sqrt <| float (Vertex.x b - Vertex.x a) ** 2.0 + float (Vertex.y b - Vertex.y a) ** 2.0
 
     /// Calculate the bearing needed to go from point a to point b, where 0.0<rad> points
     /// directly upwards.
     let angleBetween src dest =
-        1.0<rad> * atan2 (float dest.Y - float src.Y) (float dest.X - float src.X)
+        let dy = float <| Vertex.y dest - Vertex.y src
+        let dx = float <| Vertex.x dest - Vertex.x src
+        1.0<rad> * atan2 dy dx
