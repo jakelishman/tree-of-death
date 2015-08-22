64a07e5eb212c3daffe808de2b7cd5cd93f153d7
Jake Lishman <j.lishman@warwick.ac.uk>
2015-08-22 14:28:16 +0100

    Add more functional random functions

--- TreeOfDeath/Random.fs
+++ TreeOfDeath/Random.fs
@@ -14,3 +14,12 @@ module Random =
     /// Get a random non-negative fraction between 0 and 1.
     let nonNegativeFraction () =
         float (randomGenerator.Next()) / float Int32.MaxValue
+
+    /// Get a non-negative integer between 0 and Int32.MaxValue.
+    let nonNegativeInteger () = randomGenerator.Next ()
+
+    /// Get an integer between Int32.MinValue and Int32.MaxValue.
+    let integer () = randomGenerator.Next (Int32.MinValue, Int32.MaxValue)
+
+    /// Get an integer between the two specified values.
+    let integerBetween a b = randomGenerator.Next (a, b)
